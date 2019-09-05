7170f866607c58c67b560c6cddf916b51d3e2416
Ian <i.r.butterworth@gmail.com>
2019-09-05 15:35:58 -0400

    add X11 dep

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -6,15 +6,14 @@ version = v"3.3"
 
 # Collection of sources required to build glfw
 sources = [
-    "https://github.com/glfw/glfw.git" =>
-    "b0796109629931b6fa6e449c15a177845256a407",
-
+    "https://github.com/glfw/glfw/archive/3.3.tar.gz" =>
+    "81bf5fde487676a8af55cb317830703086bb534c53968d71936e7b48ee5a0f3e",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd glfw/
+cd glfw-3.3/
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=false -DGLFW_BUILD_TESTS=false
 make -j${ncore}
 make install
@@ -24,11 +23,7 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    MacOS(:x86_64),
-    Windows(:x86_64),
-    Windows(:i686),
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -36,7 +31,9 @@ products(prefix) = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = []
+dependencies = [
+    "https://github.com/giordano/Yggdrasil/releases/download/X11-v1.6.8/build_X11.v1.6.8.jl",
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, "glfw", version, sources, script, platforms, products, dependencies)
